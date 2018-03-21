fa37f69bac5507fdd0b2a78e0c57787ee338dee8
sarathkumarch <chidellasarathkumar@gmail.com>
2018-03-21 02:18:17 -0500

    ngFor and trackBy

--- hello-world/src/app/directives/directives.component.ts
+++ hello-world/src/app/directives/directives.component.ts
@@ -8,5 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent {
   courses = [2, 3, 4, 6];
   viewMode = 'map';
+  authors;
   //viewMode = 'something';
+  getAuthors() {
+    this.authors = [
+      { id:1, name:'author1' },
+      { id:2, name:'author2' },
+      { id:3, name:'author3' },
+      { id:4, name:'author4' },
+    ];
+  }
+
+  trackAuthors(index, author){
+    return author? author.id : undefined
+  }
+
+  onAdd() {
+    this.authors.push({id: 5, name:'author5'});
+  }
+
+  onRemove(author){
+    let index = this.authors.indexOf(author);
+    this.authors.splice(index, 1);
+  }
 }
