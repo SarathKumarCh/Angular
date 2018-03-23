45ea8cc250c1ae0f8b6fcf5dbd556b4be7db9da0
sarathkumarch <chidellasarathkumar@gmail.com>
2018-03-23 00:10:48 -0500

    http.post

--- hello-world/src/app/http-services/http-services.component.ts
+++ hello-world/src/app/http-services/http-services.component.ts
@@ -8,14 +8,28 @@ import { Http } from '@angular/http';
 })
 export class HttpServicesComponent {
   posts : any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http) { 
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  //get data from given url
+  constructor(private http: Http) { 
+    http.get(this.url)
       .subscribe(response => {
         this.posts = response.json();
         //console.log(response.json());
       })
   }
 
+  //post data to given url in form of JSON
+  onPostData(inputData: HTMLInputElement) {
+    let post = {title: inputData.value};
+    inputData.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+        .subscribe(response => {
+          post['id'] = response.json().id;
+          this.posts.splice(0, 0, post);
+          console.log(response.json());
+        })
+  }
 
 }
